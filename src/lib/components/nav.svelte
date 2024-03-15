2cb5c51b466a5ae97a6208d2ca5ea4359a0dd1f7
Manik Rana <manikrana54@gmail.com>
2024-03-15 19:51:05 +0530

    feat: update navbar

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -2,6 +2,14 @@
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import Logo from './logo.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { fly } from 'svelte/transition';
+
+	let isHovered = false;
+
+	function toggleHover() {
+		isHovered = !isHovered;
+	}
 
 	let isScrolled = writable(false);
 
@@ -23,21 +31,22 @@
 </script>
 
 <nav
-	class={`sticky left-0 right-0 top-0 z-50 ${isScrolledValue ? 'border-b border-b-accent' : ''}`}
+	class={`sticky left-0 right-0 top-0 z-50 flex flex-row items-center justify-between p-3 backdrop-blur-lg ${isScrolledValue ? 'border-b border-b-accent' : ''}`}
 >
-	<Logo pathClass="text-muted-foreground hover:text-white" classes="h-8 w-8" />
+	<a href="/" class="spinner text-muted-foreground transition-colors hover:text-white">
+		<Logo classes="h-5 w-5" />
+	</a>
+	<div class="flex flex-row items-center justify-center">
+		<Button variant="default" size="sm">Sign in</Button>
+	</div>
 </nav>
 
 <style>
-	.navbar {
-		position: sticky;
-		top: 0;
-		left: 0;
-		right: 0;
-		z-index: 1000;
-		/* transition: border-bottom 0.3s; */
+	.spinner {
+		transition: transform 0.7s cubic-bezier(0.075, 0.82, 0.165, 1);
 	}
-	.scrolled {
-		border-bottom: 2px solid black;
+
+	.spinner:hover {
+		transform: rotate(360deg);
 	}
 </style>
