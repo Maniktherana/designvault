a509999936da567aa96eef2455415d77c520435a
Manik Rana <manikrana54@gmail.com>
2024-04-05 13:04:25 +0530

    feat: add subscribe sheet

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -5,6 +5,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import ModeToggle from './mode-toggle.svelte';
 	import { IconMailFilled, IconBolt } from '@tabler/icons-svelte';
+	import * as Sheet from '$lib/components/ui/sheet';
 
 	let isScrolled = writable(false);
 
@@ -35,7 +36,7 @@
 		<Logo classes="h-5 w-5" />
 	</a>
 
-	<div class="nav-width flex min-w-0 flex-1 items-center justify-between">
+	<div class="nav-width flex min-w-0 flex-1 items-center justify-between font-mono">
 		<div class="flex flex-row items-center justify-center gap-3">
 			<Button href="/" variant="ghost" size="sm" class="hidden gap-1 md:flex">
 				<Logo classes="h-5 w-5" />
@@ -45,10 +46,35 @@
 				<IconBolt size="20" stroke="1.5" />
 				<span class="hidden md:block">About</span>
 			</Button>
-			<Button variant="ghost" size="sm" class="flex gap-1">
-				<IconMailFilled size="20" stroke="1.5" />
-				<span class="hidden md:block">Subscribe</span>
-			</Button>
+
+			<Sheet.Root>
+				<Sheet.Trigger>
+					<Button variant="ghost" size="sm" class="flex gap-1">
+						<IconMailFilled size="20" stroke="1.5" />
+						<span class="hidden md:block">Subscribe</span>
+					</Button>
+				</Sheet.Trigger>
+				<Sheet.Content
+					side="bottom"
+					class="max-width mx-5 my-5 h-[20vh] rounded-xl border border-muted-foreground/30 bg-muted/30 text-center backdrop-blur-xl md:mx-auto"
+				>
+					<Sheet.Header>
+						<Sheet.Description>
+							<div class="flex w-full flex-col items-center justify-center gap-3">
+								<div class="flex w-full flex-row items-center justify-center gap-3">
+									<div
+										class="flex h-8 w-8 items-center justify-center rounded-full bg-muted-foreground/30"
+									>
+										<Logo classes="h-6 w-6 text-white" />
+									</div>
+									<p class="text-lg text-foreground">Subscribe to Design Vault</p>
+								</div>
+								<p class="text-sm text-muted-foreground">No Spam. New designs every month.</p>
+							</div>
+						</Sheet.Description>
+					</Sheet.Header>
+				</Sheet.Content>
+			</Sheet.Root>
 		</div>
 
 		<div class="flex flex-row items-center justify-center gap-3">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,77 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { IconMailFilled } from '@tabler/icons-svelte';
+	import * as Sheet from '$lib/components/ui/sheet';
+	import Logo from '$lib/components/logo.svelte';
+</script>
+
 <div
 	class="dark:bg-grid-white/[0.1] bg-grid-black/[0.16] relative mx-auto flex h-[40vh] w-full items-center justify-center bg-background"
 >
 	<div
 		class="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_bottom,transparent,black)]"
 	></div>
-	<!-- <p
-		class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl"
+	<div class="shine-sm absolute inset-0 md:hidden"></div>
+	<div class="shine-lg absolute inset-0 hidden md:block"></div>
+	<div
+		class="max-width z-40 mx-auto flex flex-col items-center justify-center gap-5 px-5 pt-[3rem]"
 	>
-		Backgrounds
-	</p> -->
-	<div class="max-width z-40 mx-auto px-4 pt-[3rem]">
 		<h1 class="text-center text-4xl text-foreground/80 md:text-6xl">
 			Secure Storage for the Best Designs on the Web
 		</h1>
-		<p class="mt-6 text-center text-muted-foreground">
+		<p class="mt-6 text-center font-mono text-muted-foreground">
 			Design vault curates the best designs on the web. New websites every day.
 		</p>
+
+		<Sheet.Root>
+			<Sheet.Trigger>
+				<Button variant="default" size="default" class="flex gap-3 bg-transparent">
+					<IconMailFilled size="24" stroke="1.5" />
+					<span class="text-lg">Subscribe</span>
+				</Button>
+			</Sheet.Trigger>
+			<Sheet.Content
+				side="bottom"
+				class="max-width mx-5 my-5 h-[20vh] rounded-xl border border-muted-foreground/30 bg-muted/30 text-center backdrop-blur-xl md:mx-auto"
+			>
+				<Sheet.Header>
+					<Sheet.Description>
+						<div class="flex w-full flex-col items-center justify-center gap-3">
+							<div class="flex w-full flex-row items-center justify-center gap-3">
+								<div
+									class="flex h-8 w-8 items-center justify-center rounded-full bg-muted-foreground/30"
+								>
+									<Logo classes="h-6 w-6 text-white" />
+								</div>
+								<p class="text-lg text-foreground">Subscribe to Design Vault</p>
+							</div>
+							<p class="text-sm text-muted-foreground">No Spam. Just great designs every month.</p>
+						</div>
+					</Sheet.Description>
+				</Sheet.Header>
+			</Sheet.Content>
+		</Sheet.Root>
 	</div>
 </div>
 <div
-	class="z-10 h-[2px] bg-gradient-to-r from-transparent via-muted-foreground/30 to-transparent"
+	class="z-10 mx-auto h-[2px] max-w-screen-2xl bg-gradient-to-r from-transparent via-muted-foreground/30 to-transparent"
 ></div>
 
 <div class="z-10 h-screen"></div>
+
+<style>
+	.shine-sm {
+		background-image: radial-gradient(
+			200px 200px at 50% 100%,
+			#2d2f337b 20%,
+			rgba(40, 41, 45, 0) 100%
+		);
+	}
+	.shine-lg {
+		background-image: radial-gradient(
+			500px 200px at 50% 100%,
+			#2d2f337b 20%,
+			rgba(40, 41, 45, 0) 100%
+		);
+	}
+</style>
