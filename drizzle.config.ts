d9a8a5635c8691c6dce140607adc194d754de09b
Manik Rana <manikrana54@gmail.com>
2024-04-06 17:52:46 +0530

    chore: weird hack

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
-import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
 import { DATABASE_URL, DATABASE_AUTH_TOKEN } from '$env/static/private';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export default {
 	schema: 'src/lib/server/schema.ts',
@@ -10,7 +11,9 @@ export default {
 	strict: false,
 	verbose: true,
 	dbCredentials: {
-		url: DATABASE_URL,
-		authToken: DATABASE_AUTH_TOKEN
+		// url: process.env.DATABASE_URL!,
+		// authToken: process.env.DATABASE_AUTH_TOKEN!,
+		url: DATABASE_URL!,
+		authToken: DATABASE_AUTH_TOKEN!
 	}
 } satisfies Config;
