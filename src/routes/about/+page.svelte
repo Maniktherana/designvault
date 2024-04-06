6566b7cbbcadb6c3832430b7786ab15ba8d691fc
Manik Rana <manikrana54@gmail.com>
2024-04-06 17:46:12 +0530

    feat: add background to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,21 @@
-<section class="mx-auto max-w-screen-sm">
-	<h1 class="mt-28 font-mono text-3xl font-light">About</h1>
+<div
+	class="relative flex min-h-screen w-full items-start justify-start bg-background bg-grid-black/[0.16] dark:bg-grid-white/[0.1]"
+>
+	<div
+		class="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_bottom,transparent,black)]"
+	></div>
+	<section class="max-width z-[999] mx-auto mt-28 w-full">
+		<h1 class="font-mono text-3xl font-light">About</h1>
 
-	<div class="mt-10 flex flex-col items-start justify-start gap-2 font-mono">
-		<h2 class="text-lg font-light text-white">What?</h2>
-		<p class="text-sm text-muted-foreground">
-			Design Vault is a creative feed by <a
-				href="https://www.manikrana.dev/"
-				class="text-white hover:underline">Manik</a
-			>. We are dedicated to creating multiple top-tier websites each day, with the vision of
-			establishing our platform as a premier destination for web and interactive design.
-		</p>
-	</div>
-</section>
+		<div class="mt-10 flex flex-col items-start justify-start gap-2 font-mono">
+			<h2 class="text-lg font-light text-white">What?</h2>
+			<p class="text-sm text-muted-foreground">
+				Design Vault is a creative feed by <a
+					href="https://www.manikrana.dev/"
+					class="text-white hover:underline">Manik</a
+				>. We are dedicated to creating multiple top-tier websites each day, with the vision of
+				establishing our platform as a premier destination for web and interactive design.
+			</p>
+		</div>
+	</section>
+</div>
