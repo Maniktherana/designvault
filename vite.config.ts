b105b25a55a930af0f72d6b8a2b3085c4af7c147
Manik Rana <manikrana54@gmail.com>
2025-02-04 15:30:44 +0530

    chore:clean

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { kitRoutes } from 'vite-plugin-kit-routes';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit(), kitRoutes()]
-});
