dd6282c744ab1b45cc2a2d3d57522d01af211134
Manik Rana <manikrana54@gmail.com>
2025-02-11 19:57:27 +0530

    chore: config updates and fixes

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -12,7 +12,17 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center bg-background">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold font-mono text-gray-800 dark:text-gray-100">
+
+
+
+          <h1 
+
+
+
+
+
+
+          className="leading text-2xl font-bold font-mono text-gray-800 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
           <div className="h-[144px] w-[434px]">
--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -9,7 +9,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          {children}</Providers>
       </body>
     </html>
   );
