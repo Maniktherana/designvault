37a70411e2886e34369c738b49888f7fe31efbdd
Manik Rana <manikrana54@gmail.com>
2025-02-11 22:26:36 +0530

    chore: db configs

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -3,4 +3,4 @@ import { SQL } from "bun";
 import { drizzle } from "drizzle-orm/bun-sql";
 
 const client = new SQL(process.env.DATABASE_URL!);
-const db = drizzle({ client });
+export const db = drizzle({ client });
