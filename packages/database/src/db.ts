f351a0111382cf0f3a269a7fd1d6b111fe228c60
Manik Rana <manikrana54@gmail.com>
2025-02-12 22:31:09 +0530

    refac: update config

--- packages/database/src/db.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import { SQL } from "bun";
-import { drizzle } from "drizzle-orm/bun-sql";
-
-const client = new SQL(process.env.DATABASE_URL!);
-export const db = drizzle({ client });
